758bdb01a6c80dc3269c9bae7fe27a5d7ae07fe8
Arian Navabi <62673022+ariannavabi@users.noreply.github.com>
2021-02-26 14:28:58 +0330

    Add files via upload

---
+++ Proxy Disabler/ProxyDisablerV2.0.0.0_SourceCode/ProxyDisabler/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ProxyDisabler
+{
+    partial class Form_ProxyDisabler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_ProxyDisabler));
+            this.label_Title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_Title
+            // 
+            this.label_Title.AutoSize = true;
+            this.label_Title.Location = new System.Drawing.Point(59, 37);
+            this.label_Title.Name = "label_Title";
+            this.label_Title.Size = new System.Drawing.Size(152, 13);
+            this.label_Title.TabIndex = 0;
+            this.label_Title.Text = "Proxy Disabler by Arian Navabi";
+            // 
+            // Form_ProxyDisabler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 85);
+            this.Controls.Add(this.label_Title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form_ProxyDisabler";
+            this.Text = "Proxy Disabler";
+            this.Load += new System.EventHandler(this.Form_ProxyDisabler_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Title;
+    }
+}
+
---
+++ Proxy Disabler/ProxyDisablerV2.0.0.0_SourceCode/ProxyDisabler/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace ProxyDisabler
+{
+    public partial class Form_ProxyDisabler : Form
+    {
+        public Form_ProxyDisabler()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("wininet.dll")]
+        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+        public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        public const int INTERNET_OPTION_REFRESH = 37;
+        bool settingsReturn, refreshReturn;
+
+        /// <summary>
+        /// App Start
+        /// </summary>        
+        private void Form_ProxyDisabler_Load(object sender, EventArgs e)
+        {
+            disableProxy();
+        }
+
+        /// <summary>
+        /// This function does all the job & it is called in the form load.
+        /// </summary>
+        void disableProxy()
+        {
+            try
+            {
+                RegistryKey RegKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+                //RegKey.SetValue("ProxyServer", YourIP);
+                RegKey.SetValue("ProxyEnable", 0);
+
+                // These lines implement the Interface in the beginning of program 
+                // They cause the OS to refresh the settings, causing IP to realy update
+                settingsReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+                refreshReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+
+
+                MessageBox.Show(
+                    "Proxy is succesfully disabled.",
+                    "Proxy Disabled!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1
+                    );
+
+                Close();
+            }
+            catch (Exception exp)
+            {
+                DialogResult dr = MessageBox.Show(
+                    exp.Message,
+                    "Error!",
+                    MessageBoxButtons.RetryCancel,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1
+                    );
+
+                if (dr == DialogResult.Retry)
+                {
+                    disableProxy();
+                }
+                else
+                {
+                    Close();
+                }
+            }
+        }
+
+    }
+}
